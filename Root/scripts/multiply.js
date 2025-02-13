2e511afe53fc2ccd26206cf59a0e7f8f343ead11
Kunal Pal <kunalpal460@gmail.com>
2025-02-13 17:49:39 +0530

    Math 4 you added

---
+++ Root/scripts/multiply.js
@@ -0,0 +1,139 @@
+
+// const option1 = document.getElementById('option1');
+// const option2 = document.getElementById('option2');
+// const option3 = document.getElementById('option3');
+// const option4 = document.getElementById('option4');
+
+// const audio = document.getElementById('audio');
+
+// let answer = 0;
+
+// function generate_equation(){
+
+//     var num1 = Math.floor(Math.random() * 15 );
+
+//     var num2 = Math.floor(Math.random() * 15 );
+
+//     var dummyAnswer1 = Math.floor(Math.random() * 15 );
+//     var dummyAnswer2 = Math.floor(Math.random() * 15 );
+//     var dummyAnswer3 = Math.floor(Math.random() * 15 );
+
+//     var allAnswers = [];
+//     var switchAnswer = [];
+
+//     answer = eval(num1 * num2);
+
+//     document.getElementById("num1").innerHTML = num1;
+//     document.getElementById("num2").innerHTML = num2;
+
+//     allAnswers = [answer , dummyAnswer1 , dummyAnswer2 , dummyAnswer3];
+//     for(i = allAnswers.length; i--;){
+
+//         switchAnswer.push(allAnswers.splice(Math.ceil(Math.random() * (i+1)), 1)[0]);
+
+//     };
+
+//     option1.innerHTML = switchAnswer[0];
+//     option2.innerHTML = switchAnswer[1];
+//     option3.innerHTML = switchAnswer[2];
+//     option4.innerHTML = switchAnswer[3];
+
+// };
+
+// option1.addEventListener("click" , function(){
+
+//     if(option1.innerHTML = answer){
+//         generate_equation();
+//     }
+//     else{
+//         audio.play();
+//     }
+
+// });
+
+// option2.addEventListener("click" , function(){
+//     if(option1.innerHTML = answer){
+//         generate_equation();
+//     }
+//     else{
+//         audio.play();
+//     }
+// });
+
+// option3.addEventListener("click" , function(){
+
+//     if(option1.innerHTML = answer){
+//         generate_equation();
+//     }
+//     else{
+//         audio.play();
+//     }
+
+// });
+
+// option4.addEventListener("click" , function(){
+
+//     if(option1.innerHTML = answer){
+//         generate_equation();
+//     }
+//     else{
+//         audio.play();
+//     }
+
+// });
+
+//   generate_equation();
+
+
+
+
+const option1 = document.getElementById('option1');
+const option2 = document.getElementById('option2');
+const option3 = document.getElementById('option3');
+const option4 = document.getElementById('option4');
+
+const audio = document.getElementById('audio');
+
+let answer = 0;
+
+function generate_equation() {
+    let num1 = Math.floor(Math.random() * 8) + 5; // Ensuring a fair range (5-12)
+    let num2 = Math.floor(Math.random() * 8) + 5; // Same range to keep it reasonable
+
+    answer = num1 * num2;
+
+    document.getElementById("num1").innerHTML = num1;
+    document.getElementById("num2").innerHTML = num2;
+
+    let allAnswers = new Set();
+    allAnswers.add(answer);
+
+    while (allAnswers.size < 4) {
+        let dummyAnswer = answer + (Math.floor(Math.random() * 6) - 3); // Close values for difficulty
+        if (dummyAnswer > 0 && dummyAnswer !== answer) {
+            allAnswers.add(dummyAnswer);
+        }
+    }
+
+    let shuffledAnswers = Array.from(allAnswers).sort(() => Math.random() - 0.5);
+
+    option1.innerHTML = shuffledAnswers[0];
+    option2.innerHTML = shuffledAnswers[1];
+    option3.innerHTML = shuffledAnswers[2];
+    option4.innerHTML = shuffledAnswers[3];
+}
+
+function checkAnswer(selectedOption) {
+    if (parseInt(selectedOption.innerHTML) === answer) {
+        generate_equation();
+    } else {
+        audio.play();
+    }
+}
+
+option1.addEventListener("click", () => checkAnswer(option1));
+option2.addEventListener("click", () => checkAnswer(option2));
+option3.addEventListener("click", () => checkAnswer(option3));
+option4.addEventListener("click", () => checkAnswer(option4));
+
+generate_equation();
